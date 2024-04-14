dccee510cf2eacfd7bdab9688f1328a4fba0f115
rjranjan2112 <rjranjan2112@gmail.com>
2024-04-14 16:52:48 +0530

    Second Commit

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            Copyright &#169; 2024 Rajeev Ranjan. All right received;
+            Copyright &#169; 2024 Rajeev Ranjan. All right reserved;
         </footer>
     );
 }
