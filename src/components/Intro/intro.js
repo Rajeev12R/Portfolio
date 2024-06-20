6facea2a26a33790f8a99afb8cc8bebe626c5140
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-20 06:27:46 +0530

    Few Changes

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './intro.css';
 import bg from '../../assets/RajeevProfile.png';
-import btnImg from '../../assets/hireme.png';
 import docImg from '../../assets/bx--file-blank.svg';
 import cvFile from '../../assets/Rajeev_Ranjan_cv.pdf';
 import { Link } from 'react-scroll';
@@ -19,7 +18,7 @@ const Intro = () => {
                 <scan className="introText">I'm <span className="introName">Rajeev</span><br />Aspiring Software Engineer</scan>
                 <p className="introPara">I'm a 2nd-year B.Tech student at LPU. <br /> Skilled in web development with experience in creating<br /> visually appealing  and user-friendly websites. <br /> I am continuously learning new skills to enhance my skills.</p>
             <div className="btnContainer">
-                <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-100} duration={500}> <button className="btn"><img src={btnImg} alt="" className='btnImg' />Hire Me</button></Link>
+                <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-100} duration={500}> <button className="btn">Hire Me</button></Link>
                 <button className="btn" onClick={handleDownload}>
                         <img src={docImg} alt="" className='docImg' />View CV
                     </button>
